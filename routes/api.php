3ec4ff4af988ee87547cb4446923111cf32e813f
lruizsa <lruizsa23wg@ikzubirimanteo.com>
2024-12-16 12:15:09 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
+// Rutas para autenticación con Sanctum
 Route::post('login', [AuthController::class, 'login']);
+
+// Rutas para usuarios protegidas por Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class); // Usamos apiResource para gestionar las rutas de usuarios
+});
+
