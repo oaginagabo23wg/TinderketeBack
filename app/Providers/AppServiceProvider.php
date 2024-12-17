80952156496fec55daf7ffa79320471a5a076ce2
oaginagabo <oaginagabo23wg@ikzubirimanteo.com>
2024-12-17 08:40:34 +0000

    Api routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,44 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
+     * Define your route model bindings, pattern filters, etc.
      */
     public function boot(): void
     {
-        //
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     */
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes.
+     */
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
+
+    /**
+     * Define the "api" routes.
+     */
+    protected function mapApiRoutes(): void
+    {
+        Route::prefix('api') // Rutas con el prefijo /api
+            ->middleware('api') // Aplica el middleware 'api'
+            ->group(base_path('routes/api.php')); // Ruta de las rutas de la API
     }
 }
